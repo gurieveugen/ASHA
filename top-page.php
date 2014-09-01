16de145170cd26552d77420e3917f3b54bbe4504
gurieveugen <seejay@MacBook-Pro-SeeJay.local>
2014-09-01 14:28:00 +0300

    Revert "1.0"
    This reverts commit 1882c8fbf08462ba3acce809bc85edd006ed5abf.

--- top-page.php
+++
@@ -1,9 +0,0 @@
-
-		  <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-		  <figure><?php the_post_thumbnail(); ?></figure>
-			<?php else : ?>
-			<figure><img src="<?php echo get_template_directory_uri(); ?>/images/upload/header_page.jpg" alt=" "></figure>
-			<?php endif; ?>
-			<nav class="crumbs-nav">
-			  <span>HOME</span> &gt; <a href="#">INTERIOR PAGE HEADLINE</a>
-			</nav>
